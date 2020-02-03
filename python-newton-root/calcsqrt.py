d0e9f99e98ee3c1f690c7a8e17e3faacb5b8efe4
Moran98 <aaronmoran2411@gmail.com>
2020-02-03 12:05:04 +0000

    merge

---
+++ python-newton-root/calcsqrt.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+# Aaron Moran
+# Calculate the square root of a number 
+
+def sqrt(x):
+    """
+    Calculate the square root of argument x
+    """
+
+    # Initial guess for the square root
+    z = x / 2.0
+
+    # Continuously improve the guess
+    while abs(x -(z*z)) > 0.00001:
+       z = z - ((z*z) - x) / (2 * z)
+    return z
+
+myval = 63.0
+print("The square root of", myval,"is",sqrt(63.0))
+
+
