e9f5e00675e45ca5e48cc3e6db075dbc300141f5
Moran98 <aaronmoran2411@gmail.com>
2020-03-17 10:47:37 +0000

    Project/Folder

---
+++ Project/G00356519.py
@@ -0,0 +1,6 @@
+# Aaron Moran -  G00356519
+# Graph Theory Project
+# Create using Python a program which will carry out the Thompsons Construction
+# method on a Regular Expression to a NFA (Non-Deterministic Finite Automata).
+
+print("============GraphTheory============")
